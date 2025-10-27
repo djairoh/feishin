dacdf168dc415c19e98867d0fed9f1fee5332683
djairoh <ljxjah@mykolab.com>
2025-10-27 09:57:15 +0100

    fix: button transparency

--- build.sh
+++ build.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/bash
+set -xe
 
-export NVM_DIR=$HOME/.nvm
-source /usr/share/nvm/nvm.sh
-source /usr/share/nvm/install-nvm-exec
-source /usr/share/nvm/bash_completion
+pnpm install
+pnpm run package:linux
 
-set +x
-
-nvm use v18 || exit
-npm install --legacy-peer-deps
-npm run package
-
-cd release/build || exit
+cd dist
 sudo rm -r /usr/share/feishin/* && sudo tar -xf Feishin*x64.tar.xz -C /usr/share/feishin/ --strip-components=1
 
